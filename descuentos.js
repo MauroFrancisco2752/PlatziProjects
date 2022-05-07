dfdb9d32c26076b4e4a2fb5303d973b673d4e772
Hans <mcervantes7@ucol.mx>
2022-05-07 14:45:21 -0500

    Creación de funcion que calcula el porcentaje del descuento

--- descuentos.js
+++ descuentos.js
@@ -1,1 +1,24 @@
-alert("Hola mundo");
+/* ------------ DECLARACION DE OPERACIONES ----------------*/
+console.group("Operaciones");
+
+function formula(precioTotal, descuentoAplicado){
+    const subtotal = [precioTotal * (100 - descuentoAplicado)] / 100;
+    return subtotal;
+}
+console.groupEnd();
+
+/* ------------ LLAMAR OBJETOS ----------------*/
+console.group("Llamada de Objetos");
+
+function calcularDescuento(){
+    const inputPrecio = document.getElementById("precioTotal");
+    const inputprecioTotal = Number(inputPrecio.value);
+
+    const inputDescuento = document.getElementById("descuentoAplicado");
+    const inputdescuentoAplicado = Number(inputDescuento.value);
+
+    const resultado = formula(inputprecioTotal, inputdescuentoAplicado);
+    alert(resultado);
+}
+console.groupEnd();
+
