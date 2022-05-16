46928fc42f136e6d96e3a1cf7fb350a6529dbe8f
MauroFrancisco2752 <mcervantes7@ucol.mx>
2022-05-15 22:40:07 -0500

    promedio HTML y JS Ok

--- src/mmp.js
+++ src/mmp.js
@@ -1,29 +1,49 @@
 /* PROMEDIO */
 // FUNCION
 
-function calcularMediaAritmetica(lista) {
 
-    const sumaLista = lista.reduce(
-        function(valorAcumulado = 0, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
-        }
-    );
-    
-    const promedioLista = sumaLista / lista.length;
-    
-    return promedioLista;
+
+
+//FUNCION
+function calcularPromedio(){
+     // CICLO FOR
+    listaPromedio = Array();
+    valorCiclo = prompt("¿Cuantos datos desea agregar?: ");
+    let sumaTotal = 0;
+
+    // AGREGAR DATOS A LISTA
+    for (let i = 0; i < valorCiclo; i++){
+        listaPromedio[i] = prompt("Ingresa un valor: ");
+    }
+
+    // CONVERTIR A UN ARRAY LA LISTA
+    const resultadoPromedio = listaPromedio.map(Number);
+
+    // SUMA DE DATOS EN ARRAY
+    for (let i = 0; i < resultadoPromedio.length; i++){
+        sumaTotal += resultadoPromedio[i];
+    }
+
+/*
+    const resultado = sumaTotal / resultadoPromedio.length;
+
+    const outputPromedio = document.getElementById("rPromedio");
+    return outputPromedio.innerText = ("El promedio de los valores ingresados es: " + resultado);
+
+*/
+
+const sumaLista = resultadoPromedio.reduce(
+    function(valorAcumulado = 0, nuevoElemento){
+        return valorAcumulado + nuevoElemento;
+    }
+);
+
+const resultado = sumaLista / resultadoPromedio.length;
+
+const outputPromedio = document.getElementById("rPromedio");
+return outputPromedio.innerText = ("El promedio de los valores ingresados es: " + resultado);
+
 }  
 
-// CICLO FOR
 
-let valorCiclo = Number(prompt(""));
-let valorTotal = 0;
-var suma = 0;
 
-for(let i = 1; i <= valorCiclo; i++){
-    valorTotal = Number(prompt("Valor:" + i));
-    suma = valorTotal + suma;
-
-}
-
-calcularMediaAritmetica(suma[5, 10, 5]);
